43279102a521f80cf70a81a41b4c9c2d694161dd
rektdeckard <friedtm@gmail.com>
2020-12-12 14:39:46 -0500

    UI: refactor from class to functional component

--- src/ui.tsx
+++ src/ui.tsx
@@ -7,41 +7,18 @@ import Toolbar from "./components/Toolbar/Toolbar";
 import IconGrid from "./components/IconGrid/IconGrid";
 import Footer from "./components/Footer/Footer";
 
-// declare function require(path: string): any
-
-class App extends React.Component {
-  textbox: HTMLInputElement;
-
-  countRef = (element: HTMLInputElement) => {
-    if (element) element.value = "5";
-    this.textbox = element;
-  };
-
-  onCreate = () => {
-    const count = parseInt(this.textbox.value, 10);
-    parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", count } },
-      "*"
-    );
-  };
-
-  onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-  };
-
-  render() {
-    return (
-      <RecoilRoot>
-        <div className="app">
-          <Toolbar />
-          <Suspense fallback={<p>Loading...</p>}>
-            <IconGrid />
-          </Suspense>
-          <Footer />
-        </div>
-      </RecoilRoot>
-    );
-  }
-}
+const App: React.FC<{}> = () => {
+  return (
+    <RecoilRoot>
+      <div className="app">
+        <Toolbar />
+        <Suspense fallback={<p>Loading...</p>}>
+          <IconGrid />
+        </Suspense>
+        <Footer />
+      </div>
+    </RecoilRoot>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("react-page"));
