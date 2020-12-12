04c04753028f824f31f7ad5686f36f738ecd3ff3
rektdeckard <friedtm@gmail.com>
2020-12-12 14:37:04 -0500

    main: refactor 'insert' action and add toast notification

--- src/code.ts
+++ src/code.ts
@@ -5,26 +5,34 @@ let xOffset = 0;
 figma.ui.onmessage = ({ type, payload }) => {
   switch (type) {
     case "insert":
-      const node = figma.createNodeFromSvg(payload.svg);
-      const { x, y } = figma.viewport.center;
-      node.name = payload.name;
-      node.constrainProportions = true;
-      node.x = x;
-      node.y = y;
-      figma.viewport.center = { x: x + 32, y };
-
-      const group = figma.group(node.children, node);
-      group.name = payload.name;
-      group.expanded = false;
-      group.constrainProportions = true;
-      group.children.forEach((child) => ungroup(child, group));
-
-      figma.currentPage.selection = [node];
+      insertIcon(payload);
       break;
+    case "log":
+    default:
+      console.log("Log: ", payload);
   }
 };
 
-function ungroup(node: BaseNode, parent: GroupNode) {
+function insertIcon(payload: { name: string; svg: string }) {
+  const node = figma.createNodeFromSvg(payload.svg);
+  const { x, y } = figma.viewport.center;
+  node.name = payload.name;
+  node.constrainProportions = true;
+  node.x = x;
+  node.y = y;
+  figma.viewport.center = { x: x + 32, y };
+
+  const group = figma.group(node.children, node);
+  group.name = payload.name;
+  group.expanded = false;
+  group.constrainProportions = true;
+  group.children.forEach((child) => ungroup(child, group));
+
+  figma.currentPage.selection = [node];
+  figma.notify(`Added ${payload.name}`, { timeout: 2000 });
+}
+
+function ungroup(node: SceneNode, parent: GroupNode) {
   if (node.type === "GROUP") {
     node.children.forEach((grandchild) => {
       ungroup(grandchild, parent);
